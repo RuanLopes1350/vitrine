a7b9793d2e896179d388d9d8f67bd78dbb180fa3
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-09-11 17:51:32 -0400

    add: implementar ControllerAuth com métodos de login, logout e recuperação

---
+++ backend/src/controller/controllerAuth.ts
@@ -0,0 +1,17 @@
+import { typeLogin } from "../types/typeLogin";
+
+class ControllerAuth {
+    constructor(){
+
+    }
+    async login(usuario:typeLogin){
+
+    }
+
+    async logout(id:string){
+
+    }
+    async recover(){
+        
+    }
+}
