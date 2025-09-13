c82ecdfb93fb199a99735ea833c136b46986f51a
Luis Felipe <luis.felipe.lopes1275@gmail.com>
2025-09-12 22:08:15 -0400

    Ajustando Validação do Zod para nova versão

--- backend/src/utils/validations/usuarioSchema.ts
+++ backend/src/utils/validations/usuarioSchema.ts
@@ -3,10 +3,10 @@ import { z } from 'zod';
 const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 const UsuarioSchema = z.object({
-    nome: z.string().min(1, 'Campo nome é obrigatório.'),
-    email: z.string().email('Formato de email inválido.').min(1, 'Campo email é obrigatório.'),
-    senha: z.string().min(8, 'A senha deve ter pelo menos 8 caracteres.').regex(senhaRegex, 'A senha deve conter pelo menos 1 letra maiúscula, 1 letra minúscula, 1 número e no mínimo 8 caracteres.'),
-    whatsapp: z.string().min(1, 'Campo whatsapp é obrigatório.'),
+    nome: z.string({message: 'Campo nome é obrigatório.'}).min(3, 'Campo nome é obrigatório.'),
+    email: z.string({message: 'Campo email é obrigatório.'}).email('Formato de email inválido.').min(1, 'Campo email é obrigatório.'),
+    senha: z.string({message: 'A senha deve ter pelo menos 8 caracteres.'}).min(8, 'A senha deve ter pelo menos 8 caracteres.').regex(senhaRegex, 'A senha deve conter pelo menos 1 letra maiúscula, 1 letra minúscula, 1 número e no mínimo 8 caracteres.'),
+    whatsapp: z.string({message: 'Campo whatsapp é obrigatório.'}).min(10, 'Campo whatsapp é obrigatório.'),
     ativo: z.boolean().optional(),
 })
 
