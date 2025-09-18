6507a97e7d2ff4ad72cd9c4c083df770945ab6bf
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-09-18 17:41:50 -0400

    finalizado tela login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,13 +4,18 @@ import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <div className="bg-[#F9FAFB] h-screen flex flex-col">
-            <div className="border-2 w-[448px] h-[301px] text-[#111827]">
-                <h1>Faça login em sua conta</h1>
-                <p className="text-[#4B5563]">Ou <Link href="/cadastro" className="text-[#2563EB]">crie uma nova conta</Link></p>
-                <Input type="email" placeholder="Email" />
-                <Input type="password" placeholder="Senha" />
-                <Link href="/esqueci-minha-senha-a" className="text-[#2563EB]">Esqueceu sua senha?</Link>
+        <div className="bg-[#F9FAFB] h-screen flex flex-col justify-center items-center">
+            <div className="w-[448px] h-[301px] text-[#111827]">
+                <h1 className="text-[25.5px] pl-8">Faça login em sua conta</h1>
+                <p className="text-[#4B5563] pl-34 pb-14">Ou <Link href="/cadastro" className="text-[#2563EB]">crie uma nova conta</Link></p>
+                <div className="flex flex-col gap-8">
+                    <div className="border-2 border-[#D1D5DB] rounded-md gap-0 overflow-hidden">
+                        <Input type="email" placeholder="Email" className="w-[448px] h-[38px] border-[#D1D5DB] rounded-none focus-visible:ring-0 focus-visible:ring-offset-0" />
+                        <Input type="password" placeholder="Senha" className="w-[448px] h-[38px] border-[#D1D5DB] rounded-none focus-visible:ring-0 focus-visible:ring-offset-0" />
+                    </div>
+                    <Link href="/esqueci-minha-senha-a" className="text-[#2563EB]">Esqueceu sua senha?</Link>
+                    <Button className="w-[448px] h-[38px] bg-[#2563EB] text-white cursor-pointer">Entrar</Button>
+                </div>
             </div>
         </div>
     )
