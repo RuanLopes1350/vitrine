43888b2d269cc52318ba03a8ea034e084e5e4518
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-09-20 05:50:55 -0400

    avançado na estilização do modal

--- frontend/app/testes/page.tsx
+++ frontend/app/testes/page.tsx
@@ -7,21 +7,13 @@ export default function TestesPage() {
         <div>
             <Modal
                 titulo="Adicionar Um Novo Produto"
-                label1="Nome do Produto"
-                placeholder1="Insira o nome do produto"
-                label2="Descrição"
-                placeholder2="Insira a descrição do produto"
-                label3="Preço"
-                placeholder3="R$ 50.00"
-                label4="URL da Imagem"
-                placeholder4="Insira a URL da imagem"
                 button1={{
                     texto: "Cancelar",
                     className: "bg-blue-500 text-white",
                     action: () => console.log("Ação do botão 1")
                 }}
                 button2={{
-                    texto: "Cancelar",
+                    texto: "Salvar",
                     className: "bg-gray-500 text-white",
                     action: () => console.log("Ação do botão 2")
                 }}
--- frontend/components/modal/index.tsx
+++ frontend/components/modal/index.tsx
@@ -2,14 +2,6 @@ import { useRef, useEffect } from "react";
 
 interface ModalProps {
     titulo: string;
-    label1: string;
-    placeholder1: string;
-    label2: string;
-    placeholder2: string;
-    label3: string;
-    placeholder3: string;
-    label4: string;
-    placeholder4: string;
     button1: {
         texto: string;
         className: string;
@@ -24,7 +16,7 @@ interface ModalProps {
     onClose: () => void;
 }
 
-export default function Modal({ titulo, label1, placeholder1, label2, placeholder2, label3, placeholder3, label4, placeholder4, button1, button2, isOpen, onClose }: ModalProps) {
+export default function Modal({ titulo, button1, button2, isOpen, onClose }: ModalProps) {
     const dialogRef = useRef<HTMLDialogElement>(null);
     isOpen = true;
 
@@ -49,17 +41,27 @@ export default function Modal({ titulo, label1, placeholder1, label2, placeholde
 
 
     return (
-        <dialog ref={dialogRef} onCancel={onClose} className="w-full max-w-lg rounded-xl p-0 shadow-2xl backdrop:bg-gray-900/60 backdrop:blur-sm fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div className="w-[448px] h-[658px]">
-                <h2>{titulo}</h2>
-                <h4>{label1}</h4>
-                <input type="text" id="input1" placeholder={placeholder1} />
-                <h4>{label2}</h4>
-                <input type="text" id="input2" placeholder={placeholder2} />
-                <h4>{label3}</h4>
-                <input type="text" id="input3" placeholder={placeholder3} />
-                <h4>{label4}</h4>
-                <input type="text" id="input4" placeholder={placeholder4} />
+        <dialog ref={dialogRef} onCancel={onClose} className="w-full max-w-lg rounded-xl p-0 shadow-2xl backdrop:bg-gray-900/60 backdrop:blur-sm fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+            <div className="w-lg h-[658px]">
+                <div className="flex flex-col items-start justify-center bg-gradient-to-r from-[#9333EA] to-[#4338CA] w-[100%] h-[68px]">
+                    <h2 className="pl-4 text-white font-bold text-[17px] select-none">{titulo}</h2>
+                </div>
+                <h4 className="text-[#374151] font-medium text-[11.9px]">Nome do Produto</h4>
+                <input type="text" id="input1" placeholder="Insira o nome do produto" />
+                <h4 className="text-[#374151] font-medium text-[11.9px]">Descrição</h4>
+                <input type="text" id="input2" placeholder="Insira a descrição do produto" />
+                <h4 className="text-[#374151] font-medium text-[11.9px]">Preço</h4>
+                <input
+                    type="number"
+                    id="input3"
+                    placeholder="R$ 50.00"
+                    className="
+                                [-moz-appearance:textfield] /* Para Firefox */ 
+                                [&::-webkit-outer-spin-button]:appearance-none /* Para WebKit */ 
+                                [&::-webkit-inner-spin-button]:appearance-none /* Para WebKit */"
+                />
+                <h4 className="text-[#374151] font-medium text-[11.9px]">URL da Imagem</h4>
+                <input type="text" id="input4" placeholder="Insira a URL da imagem" />
                 <button className={button1.className}>{button1.texto}</button>
                 {button2 && <button className={button2.className}>{button2.texto}</button>}
             </div>
