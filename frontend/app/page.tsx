a32a87153ae4dc6bb3d474fb4acccdd98591148c
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-09-27 22:59:11 -0400

    tentando resolver erros para deploy na vercel

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,11 +8,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    // Esta lógica agora é segura, pois só roda no cliente após a montagem.
     router.replace('/login');
   }, [router]);
-
-  // Renderiza um placeholder nulo ou uma tela de carregamento
-  // enquanto o redirecionamento acontece.
   return null;
 }
