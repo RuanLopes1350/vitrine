1be1fb7507c19ed6da29534532f666769c0cca64
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-09-29 18:03:52 -0400

    alteração em seeds para gerar produtos de um usuario especifico para testes

--- backend/src/seeds/seedProduto.ts
+++ backend/src/seeds/seedProduto.ts
@@ -1,4 +1,5 @@
 import modelProduto from '../models/modelProduto.js'
+import modelUsuario from '../models/modelUsuario.js';
 import { typeProduto } from '../types/typeProduto.js'
 import { typeUsuario } from '../types/typeUsuario.js';
 
@@ -7,8 +8,30 @@ import { getGlobalFakeMapping } from './globalFakeMapping.js'
 async function seedProdutos(usuarios: typeUsuario[]) {
     await modelProduto.collection.deleteMany();
 
+    const usuarioFixo = await modelUsuario.findOne({ email: 'intel.spec.lopes@gmail.com' });
+    if (!usuarioFixo) {
+        throw new Error("Usuário fixo com email 'intel.spec.lopes@gmail.com' não encontrado.");
+    }
+
+    const mapping = await getGlobalFakeMapping();
+
+    let quantidade: number = 15;
+
+    for(let criados: number = 0; criados < quantidade; criados++){
+        const produtoAleatorio: typeProduto = {
+            criador: usuarioFixo._id,
+            nome_produto: mapping.produto.nome_produto(),
+            descricao: mapping.produto.descricao(),
+            mensagem: mapping.produto.mensagem(),
+            imagem: mapping.produto.imagem(),
+            preco: Number(mapping.produto.preco()),
+            ativo: mapping.produto.ativo(),
+        };
+        await modelProduto.collection.insertOne(produtoAleatorio);
+    }
+
     const produtoFixo: typeProduto = {
-        criador: 'Ruan Lopes',
+        criador: usuarioFixo._id,
         nome_produto: 'Produto de Teste',
         descricao: 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         mensagem: `Olá, me interessei pelo produto X...`,
@@ -16,10 +39,9 @@ async function seedProdutos(usuarios: typeUsuario[]) {
         preco: 123.45,
         ativo: true
     }
+
     await modelProduto.collection.insertOne(produtoFixo);
     console.log('Produto pré-definido cadastrado com sucesso!')
-
-    const mapping = await getGlobalFakeMapping();
 
     const produtosAleatorios: typeProduto[] = [];
 
