d1dc3b8b5a7d41d164dd34ad21709a80659ce452
Silvio Huan <silviohuan@gmail.com>
2025-10-01 17:02:18 -0300

    finalizado a lógica de buscar os dados da API corretamente

--- frontend/app/[loja]/page.tsx
+++ frontend/app/[loja]/page.tsx
@@ -1,30 +1,21 @@
 "use client"
-import { useRouter } from "next/router";
 import { useAuth } from "@/contexts/AuthContext";
 import { useEffect, useState } from "react";
+import { useProdutosLoja } from "@/hooks/useLoja";
+import { usePathname } from "next/navigation";
 
 export default function PageLoja(){
-    const {user, logout} = useAuth()
-    const [nomeLoja, setIsNomeLoja] = useState<string>()
-    const [idUser, setIsIdUser] = useState<string>()
-    async function getLoja(nome:string, id:string) {
-        try{
-
-            
-
-        }catch(erro){
-            console.log(erro)
-        }
-    }
+    const {produtos, getProdutos} = useProdutosLoja()
+    const [id, getId] = useState<string>("")
+    const local = usePathname()
     useEffect(()=>{
-        if(user){
-            setIsNomeLoja(user.nomeLoja)
-            setIsIdUser(user.id)
-        }
+        const id = local.match(/[0-9a-fA-F]{24}/)?.[0] as string
+        getProdutos(id)
     },[])
+
     return(
         <div>
-            Está loja está online
+            {produtos?.data.docs[0].criador.nomeLoja}
         </div>
     )
 }
