50e6a06f7604d2711259e555687e06d056553b1b
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-10-06 17:06:58 -0400

    modificação menor em card de produto na tela publica

--- backend/src/repository/repositoryUsuario.ts
+++ backend/src/repository/repositoryUsuario.ts
@@ -42,7 +42,6 @@ class RepositoryUsuario {
 
     async buscarPorEmail(email:string) {
         const data = await this.model.findOne({email:email}, ' +fotoPerfil +senha +nomeLoja +whatsapp +ativo' );
-        console.log(`Debugando: Retornando \n${data}`)
         return data
     }
 
