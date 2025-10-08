f1d888ff29bcf44f2ab305810577c49d61341a5b
Silvio Huan <silviohuan@gmail.com>
2025-10-08 12:20:59 -0300

    padronizado distribuição dos cards para ficarem igual a da página de inicio

--- frontend/app/[loja]/page.tsx
+++ frontend/app/[loja]/page.tsx
@@ -123,7 +123,7 @@ export default function PageLoja() {
                     {/* Grid de produtos */}
                     {temProdutos ? (
                         <>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                                 {currentItems.map((produto) => (
                                     <ProdutoCardLoja
                                         key={produto._id}
