859572d71087d43243f5b55273cec95bb4677008
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-10-09 21:03:49 -0400

    construindo lógica para recuperar senha

---
+++ frontend/hooks/useSenha.ts
@@ -0,0 +1,12 @@
+import apiClient from "@/apiClient";
+
+
+export function Codigo() {
+    async function getCodigo(codigo:string) {
+        try{
+            // const response = apiClient(`/api/senha/${codigo}`)
+        }catch(erro){
+
+        }
+    }
+}
