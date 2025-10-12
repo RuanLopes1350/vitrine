5846f2cc1d41e7e965df163b37a3541cf1dafed2
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-10-12 05:48:13 -0400

    Atualizado metadados da aplicação para melhorar SEO e compartilhamento em redes sociais

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,19 +6,32 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-  title: 'Vitrine',
+  title: 'Vitrine - Sua loja online',
+  description: 'Crie sua vitrine de produtos e compartilhe com seus clientes de forma simples e direta.',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
     apple: '/favicon.ico',
   },
-}
+  openGraph: {
+    title: 'Vitrine - Sua loja online',
+    description: 'Crie sua vitrine de produtos e compartilhe com seus clientes de forma simples e direta.',
+    url: 'https://vitrine-fawn.vercel.app/',
+    siteName: 'Vitrine',
+    images: [
+      {
+        url: 'https://i.imgur.com/wGm9Y07.png',
+        width: 1200,
+        height: 630,
+        alt: 'Logo do projeto Vitrine',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+};
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="pt-BR">
       <body className="h-screen flex flex-col">
